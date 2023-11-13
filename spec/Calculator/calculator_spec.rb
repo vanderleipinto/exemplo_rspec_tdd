6cc4ad92cb87ad13d4be2647f7a312a03ebd0b1e
vanderleipinto <vandecopinto@gmail.com>
2023-11-13 16:56:02 -0300

    Até a aula 12

---
+++ spec/Calculator/calculator_spec.rb
@@ -0,0 +1,22 @@
+require "calculator"
+describe Calculator do
+  context "#sum" do
+    it 'use sum method' do
+      calc = Calculator.new
+      result = calc.sum(5,7)
+      expect(result).to eq (12)
+    end
+
+    specify 'with negative numbers' do
+      calc = Calculator.new
+      result = calc.sum(-5,7)
+      expect(result).to eq (2)
+    end
+
+    example 'with two negative numbers' do
+      calc = Calculator.new
+      result = calc.sum(-5,-7)
+      expect(result).to eq (-12)
+    end
+  end
+end
