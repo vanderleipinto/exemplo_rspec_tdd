958e96611ec8d5af48e88eeaa5486f925e2cd91d
vanderleipinto <vandecopinto@gmail.com>
2023-12-04 20:12:16 -0300

    Added Shared Examples

--- spec/shared_examples/shared_examples_rspec.rb
+++ spec/shared_examples/shared_examples_rspec.rb
@@ -1,18 +1,18 @@
 require 'pessoa'
 
+shared_examples 'status' do |sentimento|
+  it "#{sentimento}" do 
+    pessoa.send("#{sentimento}!")
+    expect(pessoa.status).to eq ("Sentindo-se #{sentimento.capitalize}!")
+  end
+
+end
+
 describe 'Pessoa' do 
   subject(:pessoa) {Pessoa.new}
-
-  pessoa.feliz!
   
-  it 'feliz!' do 
-    pessoa.feliz!
-    expect(pessoa.status).to eq ("Sentindo-se Feliz!")
-  end
+  include_examples 'status', :feliz
+  it_behaves_like 'status', :triste
+  it_should_behave_like 'status', :contente
   
-  it 'triste!' do 
-    pessoa.triste!
-    expect(pessoa.status).to eq ("Sentindo-se Triste!")
-  end
-
 end
