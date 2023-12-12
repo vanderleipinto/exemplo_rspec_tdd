838521fae16d90c2aec3ce05783f484ac63f59a9
vanderleipinto <vandecopinto@gmail.com>
2023-12-12 14:47:41 -0300

    Doubles

---
+++ spec/test_doubles/user_spec.rb
@@ -0,0 +1,10 @@
+describe 'Test Double' do
+  it '' do
+    user = double('User')
+    # allow(user).to receive_messages(name: 'Jack', password: 'secret')
+    allow(user).to receive(:name).and_return('Jack')
+    allow(user).to receive(:password).and_return('secret')
+    puts user.name 
+    puts user.password
+  end
+end
