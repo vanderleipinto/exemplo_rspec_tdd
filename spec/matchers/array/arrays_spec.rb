dce0b9e3dfd6da43d3e7e06efa8bcc5603b12deb
vanderleipinto <vandecopinto@gmail.com>
2023-12-11 21:08:04 -0300

    Tag Filters

--- spec/matchers/array/arrays_spec.rb
+++ spec/matchers/array/arrays_spec.rb
@@ -1,9 +1,9 @@
-describe [1,2,3] do
+describe [1,2,3], type: 'collection' do
   it 'Array to #include 2 and 1' do
     expect(subject).to include(2,1)
   end
   
-  it 'Array to #contain_exactly' do
+  it 'Array to #contain_exactly', :slow do
     expect(subject).to contain_exactly(1,3,2)
   end
   
