5ca06a09dea1e0535f8a5803ca93aebb5603c5fe
vanderleipinto <vandecopinto@gmail.com>
2023-12-13 16:56:15 -0300

    Doing Stubs

---
+++ spec/stubs/stubs_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../../lib/student'
+require_relative '../../lib/course'
+
+describe 'Stub' do
+  it 'has_finished?' do
+    student = Student.new
+    course = Course.new
+
+    allow(student).to receive(:has_finished?).with(an_instance_of(Course)).and_return(true)
+
+    course_finished = student.has_finished?(course)
+
+    expect(course_finished).to be_truthy
+  end
+end
