d1ea8b64a57f2e06a038ad07c726bab774fc5936
vanderleipinto <vandecopinto@gmail.com>
2023-12-15 07:32:42 -0300

    As null object

--- spec/test_doubles/user_spec.rb
+++ spec/test_doubles/user_spec.rb
@@ -7,4 +7,14 @@ describe 'Test Double' do
     puts user.name 
     puts user.password
   end
+
+  it 'as null_object' do
+    user = double('User').as_null_object
+    # allow(user).to receive_messages(name: 'Jack', password: 'secret')
+    allow(user).to receive(:name).and_return('Jack')
+    allow(user).to receive(:password).and_return('secret')
+    puts user.name 
+    puts user.password
+    user.tutu
+  end
 end
