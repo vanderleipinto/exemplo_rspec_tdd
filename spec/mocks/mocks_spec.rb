65ad6f07c4f32e5827a3fe4c42d678a0b1811199
vanderleipinto <vandecopinto@gmail.com>
2023-12-15 07:26:10 -0300

    Mock Expectations

--- spec/mocks/mocks_spec.rb
+++ spec/mocks/mocks_spec.rb
@@ -12,5 +12,36 @@ describe 'Stub' do
     # exercise
     student.bar
   end 
+   
+  it 'args' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123)    
+    student.foo(123)
+    
+end
+  it 'Repetição' do
+    student = Student.new
+    
+    # expect(student).to receive(:foo).once
+    expect(student).to receive(:foo).twice
+    # expect(student).to receive(:foo).excatly(3).times
+    # expect(student).to receive(:foo).at_least(:once)
+    # expect(student).to receive(:foo).at_least(:twice)
+    # expect(student).to receive(:foo).at_least(n).times
+
+    student.foo(123)
+    student.foo(123)
+    
+end
+  it 'Com retorno' do
+    student = Student.new
+    
+     expect(student).to receive(:foo).with(123).and_return(false)
+    
+
+    student.foo(123)
+    
+    
+end
 
 end
--- spec/stubs/stubs_spec.rb
+++ spec/stubs/stubs_spec.rb
@@ -41,7 +41,6 @@ describe 'Stub' do
     allow(student).to receive(:bar).and_raise(RuntimeError)
 
     expect{student.bar}.to raise_error(RuntimeError)
-    expect(student).to receive(:bar)
-    
   end
+   
 end
